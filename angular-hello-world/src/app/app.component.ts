f4366878f21615e5668c9f465ab7b0dd7579e5a2
GabriWasef <wasef.gabriele@iisgalvanimi.edu.it>
2026-01-22 12:19:35 +0100

    Remove unused RouterOutlet import

--- angular-hello-world/src/app/app.component.ts
+++ angular-hello-world/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponentComponent } from './hello-world-component/hello-world-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HelloWorldComponentComponent],
+  imports: [CommonModule, HelloWorldComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
